5cbd4a043e83c1e8d125c3476dfc3775506179c6
Jyothy Dulam <jxdulam@shockers.wichita.edu>
2025-02-23 10:12:45 -0600

    Delete product_data.jsx

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,7 +1,7 @@
 import { createContext,useState,useEffect} from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
-import products from '../assets/product_data';
+import { products } from '../assets/assets'; 
 
 
 export const ShopContext = createContext();
